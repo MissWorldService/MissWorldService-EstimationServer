21da9e4fae37cdf1c1c4c46b1238a8bcd7f9aa44
Aidar Garikhanov <a1d4r@yandex.ru>
2020-11-29 13:53:19 +0300

    Create an endpoint for uploading testset given URL

--- README.md
+++ README.md
@@ -8,9 +8,7 @@ cd app/
 ### Run TensorFlow Serving
 Start container and open the REST API port:
 ```
-docker run -t --rm -p 8501:8501 \
-    -v "$(pwd)/model:/models/model/1" \
-    tensorflow/serving &
+docker run -t --rm -p 8501:8501 -v "$(pwd)/model:/models/model/1" tensorflow/serving
 ```
 ### Run Flask application
 Export environment variable:
--- app/app.py
+++ app/app.py
@@ -1,10 +1,22 @@
 from flask import Flask, request
+from werkzeug.local import Local
+import requests
+import numpy
+import zipfile 
+import io
+
+import model_metrics
+
 app = Flask(__name__)
+loc = Local()
+loc.busy = False
 
 
 @app.route('/ping/')
 def ping():
-    pass
+    return {
+        'status': 'busy' if loc.busy else 'ready',
+    }
 
 @app.route('/upload-testset/', methods=['POST'])
 def upload_testset():
@@ -12,7 +24,21 @@ def upload_testset():
 
 @app.route('/upload-testset-from-url/', methods=['POST'])
 def upload_testset_from_url():
-    pass
+    try:
+        url = request.json['url']
+        r = requests.get(url, stream=True)
+        with zipfile.ZipFile(io.BytesIO(r.content)) as z:
+            z.extractall('testset/')
+    except Exception as e:
+        app.logger.error(repr(e))
+        return {
+            'status': 'error',
+            'message': str(e),
+        }, 500
+    else:
+        return {
+            'status': 'ok'
+        }
 
 @app.route('/upload-model/', methods=['POST'])
 def upload_model():
@@ -22,6 +48,9 @@ def upload_model():
 def upload_model_from_url():
     pass
 
-@app.route('/evaluate/', methods=['GET']):
+@app.route('/evaluate/', methods=['GET'])
 def evaluate_model():
     pass
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
